52a5b00476733212cc37c50f901c267af60fb728
Rayxez <dviettai204@gmail.com>
2025-07-14 11:10:43 +0700

    Form AddSV , Form EditSV

--- QuanLyHoSoSinhVien/QuanLyHoSoSinhVien/PresentationLayer/view/ThemSV.Designer.cs
+++ QuanLyHoSoSinhVien/QuanLyHoSoSinhVien/PresentationLayer/view/ThemSV.Designer.cs
@@ -28,18 +28,455 @@
         /// </summary>
         private void InitializeComponent()
         {
+            groupBox1 = new GroupBox();
+            guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            groupBox2 = new GroupBox();
+            btnThemSV = new FontAwesome.Sharp.IconButton();
+            btnHuy = new FontAwesome.Sharp.IconButton();
+            label1 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label12 = new Label();
+            label11 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            label13 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            comboBox2 = new ComboBox();
+            textBox11 = new TextBox();
+            label14 = new Label();
+            textBox12 = new TextBox();
+            label15 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox12);
+            groupBox1.Controls.Add(label15);
+            groupBox1.Controls.Add(textBox11);
+            groupBox1.Controls.Add(label14);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(textBox10);
+            groupBox1.Controls.Add(textBox9);
+            groupBox1.Controls.Add(textBox8);
+            groupBox1.Controls.Add(textBox7);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label13);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(label12);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            groupBox1.Location = new Point(33, 124);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(968, 379);
+            groupBox1.TabIndex = 14;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "NHẬP THÔNG TIN";
+            // 
+            // guna2HtmlLabel2
+            // 
+            guna2HtmlLabel2.AutoSize = false;
+            guna2HtmlLabel2.BackColor = Color.Transparent;
+            guna2HtmlLabel2.Location = new Point(214, 24);
+            guna2HtmlLabel2.Margin = new Padding(2);
+            guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            guna2HtmlLabel2.Size = new Size(593, 95);
+            guna2HtmlLabel2.TabIndex = 16;
+            guna2HtmlLabel2.Text = "<h1 style=\"font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;font-weight: bold;\">HỆ THỐNG QUẢN LÍ HỒ SƠ SINH VIÊN</h1>\r\n";
+            guna2HtmlLabel2.TextAlignment = ContentAlignment.TopCenter;
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.AutoSize = false;
+            guna2HtmlLabel1.BackColor = Color.Transparent;
+            guna2HtmlLabel1.Location = new Point(194, 54);
+            guna2HtmlLabel1.Margin = new Padding(2);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(126, 22);
+            guna2HtmlLabel1.TabIndex = 15;
+            guna2HtmlLabel1.Text = "<h1 style=\"font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;font-weight: bold;\">HỆ THỐNG QUẢN LÍ HỒ SƠ SINH VIÊN</h1>";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnHuy);
+            groupBox2.Controls.Add(btnThemSV);
+            groupBox2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            groupBox2.Location = new Point(34, 537);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(975, 117);
+            groupBox2.TabIndex = 17;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "CHỨC NĂNG";
+            // 
+            // btnThemSV
+            // 
+            btnThemSV.BackColor = Color.LimeGreen;
+            btnThemSV.FlatAppearance.BorderSize = 0;
+            btnThemSV.FlatStyle = FlatStyle.Flat;
+            btnThemSV.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btnThemSV.ForeColor = SystemColors.ButtonHighlight;
+            btnThemSV.IconChar = FontAwesome.Sharp.IconChar.None;
+            btnThemSV.IconColor = Color.Black;
+            btnThemSV.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnThemSV.IconSize = 25;
+            btnThemSV.ImageAlign = ContentAlignment.MiddleLeft;
+            btnThemSV.Location = new Point(231, 44);
+            btnThemSV.Name = "btnThemSV";
+            btnThemSV.RightToLeft = RightToLeft.No;
+            btnThemSV.Size = new Size(165, 38);
+            btnThemSV.TabIndex = 5;
+            btnThemSV.Text = "+ Thêm sinh viên mới";
+            btnThemSV.UseVisualStyleBackColor = false;
+            // 
+            // btnHuy
+            // 
+            btnHuy.BackColor = Color.Red;
+            btnHuy.FlatAppearance.BorderSize = 0;
+            btnHuy.FlatStyle = FlatStyle.Flat;
+            btnHuy.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btnHuy.ForeColor = SystemColors.HighlightText;
+            btnHuy.IconChar = FontAwesome.Sharp.IconChar.Trash;
+            btnHuy.IconColor = Color.Black;
+            btnHuy.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnHuy.ImageAlign = ContentAlignment.MiddleLeft;
+            btnHuy.Location = new Point(555, 44);
+            btnHuy.Name = "btnHuy";
+            btnHuy.Size = new Size(165, 38);
+            btnHuy.TabIndex = 9;
+            btnHuy.Text = "Hủy";
+            btnHuy.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 66);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Mã sinh viên ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 127);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Tên sinh viên";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(27, 200);
+            label4.Name = "label4";
+            label4.Size = new Size(79, 20);
+            label4.TabIndex = 2;
+            label4.Text = "Ngày sinh";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(27, 268);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Giới tính";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(373, 73);
+            label8.Name = "label8";
+            label8.Size = new Size(56, 20);
+            label8.TabIndex = 4;
+            label8.Text = "Địa chỉ";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(354, 134);
+            label7.Name = "label7";
+            label7.Size = new Size(100, 20);
+            label7.TabIndex = 5;
+            label7.Text = "Số điện thoại";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(391, 195);
+            label6.Name = "label6";
+            label6.Size = new Size(63, 20);
+            label6.TabIndex = 6;
+            label6.Text = "Dân tộc";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(391, 256);
+            label5.Name = "label5";
+            label5.Size = new Size(69, 20);
+            label5.TabIndex = 7;
+            label5.Text = "Tôn giáo";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(675, 69);
+            label12.Name = "label12";
+            label12.Size = new Size(47, 20);
+            label12.TabIndex = 9;
+            label12.Text = "Email";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(675, 130);
+            label11.Name = "label11";
+            label11.Size = new Size(47, 20);
+            label11.TabIndex = 10;
+            label11.Text = "CCCD";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(675, 191);
+            label10.Name = "label10";
+            label10.Size = new Size(67, 20);
+            label10.TabIndex = 11;
+            label10.Text = "Nơi sinh";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(662, 265);
+            label9.Name = "label9";
+            label9.Size = new Size(80, 20);
+            label9.TabIndex = 12;
+            label9.Text = "Trạng thái";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(48, 325);
+            label13.Name = "label13";
+            label13.Size = new Size(39, 20);
+            label13.TabIndex = 13;
+            label13.Text = "Lớp ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(128, 66);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(181, 27);
+            textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(131, 132);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(178, 27);
+            textBox2.TabIndex = 15;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(107, 195);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(260, 27);
+            dateTimePicker1.TabIndex = 16;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(128, 265);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 28);
+            comboBox1.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(457, 66);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(191, 27);
+            textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(457, 131);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(191, 27);
+            textBox4.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(480, 198);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(131, 27);
+            textBox5.TabIndex = 20;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(480, 261);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(131, 27);
+            textBox6.TabIndex = 21;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(751, 69);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(193, 27);
+            textBox7.TabIndex = 22;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(751, 134);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(193, 27);
+            textBox8.TabIndex = 23;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(752, 202);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(192, 27);
+            textBox9.TabIndex = 24;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(117, 322);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(192, 27);
+            textBox10.TabIndex = 25;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(751, 265);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(131, 28);
+            comboBox2.TabIndex = 26;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(456, 322);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(192, 27);
+            textBox11.TabIndex = 28;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(387, 325);
+            label14.Name = "label14";
+            label14.Size = new Size(56, 20);
+            label14.TabIndex = 27;
+            label14.Text = "Ngành";
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(751, 325);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(192, 27);
+            textBox12.TabIndex = 30;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(682, 328);
+            label15.Name = "label15";
+            label15.Size = new Size(45, 20);
+            label15.TabIndex = 29;
+            label15.Text = "Khoa";
             // 
             // ThemSV
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(765, 464);
+            ClientSize = new Size(1056, 681);
+            Controls.Add(groupBox2);
+            Controls.Add(guna2HtmlLabel2);
+            Controls.Add(guna2HtmlLabel1);
+            Controls.Add(groupBox1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "ThemSV";
             Text = "ThemSV";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+        private GroupBox groupBox1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private GroupBox groupBox2;
+        private FontAwesome.Sharp.IconButton btnThemSV;
+        private FontAwesome.Sharp.IconButton btnHuy;
+        private ComboBox comboBox2;
+        private TextBox textBox10;
+        private TextBox textBox9;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label13;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label3;
+        private Label label4;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox12;
+        private Label label15;
+        private TextBox textBox11;
+        private Label label14;
     }
 }
